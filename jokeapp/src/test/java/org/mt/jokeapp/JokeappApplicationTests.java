d332132b5601bf124e918297e1068528240978fe
fingerdust <mtspark@gmail.com>
2019-10-20 19:39:25 +0100

    First Commit

---
+++ jokeapp/src/test/java/org/mt/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mt.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
